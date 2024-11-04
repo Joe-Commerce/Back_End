8e61c993999ef151c9bff8a19660bf789472da6f
JClerve <joesonclerve9@gmail.com>
2024-11-04 23:29:33 +0530

    Get all the product completed and get by id is on progress

--- routes/product.js
+++ routes/product.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { getProducts, newProduct } = require("../controllers/productController");
 
-//router.get("/products", getProducts);
+router.get("/products", getProducts);
 
 router.post("/product/new", newProduct);
 
