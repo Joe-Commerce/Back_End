9cdb9cebb57b824cb8679f0c7483dd2699ca2454
JClerve <joesonclerve9@gmail.com>
2024-11-12 12:52:20 +0530

    With error handling Middleware

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const errorMiddleware = require("../back-end/middlewares/error");
 
 const products = require("./routes/product");
 
 app.use(express.json()); // Include middleware for JSON handling
 app.use("/api/v1", products); // Setup route for products
+
+app.use(errorMiddleware);
 
 module.exports = app;
