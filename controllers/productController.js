e5e0c33c450ceb266fc6b4840e4033a98331cf22
JClerve <joesonclerve9@gmail.com>
2024-11-15 21:06:12 +0530

    Take the uRL and filter the product with category

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,7 +4,9 @@ const catchAsyncError = require("../middlewares/catchAsyncError");
 const APIFeatures = require("../utils/apiFeatures");
 //Get Products  - /api/v1/products
 exports.getProducts = catchAsyncError(async (req, res, next) => {
-  const apiFeatures = new APIFeatures(Product.find(), req.query).search();
+  const apiFeatures = new APIFeatures(Product.find(), req.query)
+    .search()
+    .filter();
   const products = await apiFeatures.query;
   res.status(200).json({
     sucess: true,
