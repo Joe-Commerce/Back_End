70227d0f989b8b1e123437ba3007214c1ba66397
JClerve <joesonclerve9@gmail.com>
2024-12-25 23:45:08 +0530

    update model

--- routes/order.js
+++ routes/order.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const { isAuthenticatedUser } = require("../middlewares/authenticate");
 
 const { newOrder } = require("../controllers/orderController");
 const { isAuthenticatedUser } = require("../middlewares/authenticate");
