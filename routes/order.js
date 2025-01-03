be1c6573e3348e774ef80b76755e505b61a28945
JClerve <joesonclerve9@gmail.com>
2025-01-03 23:41:28 +0530

    get the orders router part is added

--- routes/order.js
+++ routes/order.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { newOrder, getSingleOrder } = require("../controllers/orderController");
+const {
+  newOrder,
+  getSingleOrder,
+  myOrders,
+} = require("../controllers/orderController");
 const { isAuthenticatedUser } = require("../middlewares/authenticate");
 
 router.post("/order/new", isAuthenticatedUser, newOrder);
 router.get("/order/id", isAuthenticatedUser, getSingleOrder);
+router.get("/myOrders", isAuthenticatedUser, myOrders);
 
 module.exports = router;
